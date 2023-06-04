275a7b377281e04a408b0a83daa4b6189e43165b
marceloaguillar <marceloaguillarjr@gmail.com>
2023-06-04 20:47:44 -0300

    Criação dos sites para acessar as pesquisas e votar nelas

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,5 @@
+from datetime import timezone
+import datetime
 from django.db import models
 
 # Create your models here.
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from polls.models import Question
+# Create your views here.               
 
 
 def index(request):
-    return HttpResponse ('Olá, esse é o meu primeiro site')
+    latest_question_list = Question.objects.order_by('pub_date')[:5] #PEGA TODAS AS PERGUNTAS 
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+                                            
+def results(request, question_id):
+    question = Question(pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except KeyError:
+        return render(request, 'polls/vote.html', {
+            'question': question,
+            'error_message': "Selecione uma opção"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
